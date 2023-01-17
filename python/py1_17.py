380b99b618c9ef453ba588ef5bfb9bc786c26f5e
unknown <dnpffldxjs12@gmail.com>
2023-01-17 09:57:43 +0900

    10;00

---
+++ python/py1_17.py
@@ -0,0 +1,12 @@
+'''입력 받아서 홀짝 여부 판별'''
+# num = int(input())
+# if num % 2: # if num % 2 == 1 ## 1은 True
+#     print('홀')
+# else:
+#     print('짝')
+
+'''조건 표현식'''
+# 홀짝 구분 조건표현식으로
+num = 124
+result = '홀' if num % 2 else '짝'
+print(result)
