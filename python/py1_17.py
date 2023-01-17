dd75f7e9fd63f3b5874b9e1c8ad901bf18c369af
unknown <dnpffldxjs12@gmail.com>
2023-01-17 10:54:43 +0900

    11;00

--- python/py1_17.py
+++ python/py1_17.py
@@ -10,3 +10,39 @@
 num = 124
 result = '홀' if num % 2 else '짝'
 print(result)
+
+'''for문 문자열 순회, 딕셔너리 순회'''
+# chars = input()
+
+# for char in chars:
+#     print(char)
+
+# grades = {'a': 80, 'b': 90, 'c': 83}
+# for student in grades:
+#     print(student,grades[student])
+
+# for student, grade in grades.items(): #딕셔너리.items = 튜플로 키, 값을 넘겨줌 
+#     print(student, grade)
+
+'''enumerate'''
+members = ['a','b','c']
+for idx, number in enumerate(members):
+    print(idx,number)
+
+'''list comprehension'''
+# cubic = []
+# for number in range(1,4):
+#     cubic.append(number ** 3)
+# print(cubic)
+
+cubic_list = [number ** 3 for number in range(1,4)]
+print(cubic_list)
+
+'''dict comprehension'''
+# cubic = {}
+# for number in range(1,4):
+#     cubic[number] = number ** 3
+# print(cubic)
+
+cubic_dict = {number: number ** 3 for number in range(1,4)}
+print(cubic_dict)
