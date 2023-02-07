95a74ed63438184ab3ae2d12fd759c8e8086b439
unknown <dnpffldxjs12@gmail.com>
2023-02-07 11:00:06 +0900

    11

--- algorithm/search.py
+++ algorithm/search.py
@@ -1,4 +1,4 @@
-## 델타이용 2차원 서치
+## 델타이용 2차원 탐색
 
 di = [0, 1, 0, -1]  # 우하좌상
 dj = [1, 0, -1, 0]
@@ -12,5 +12,55 @@ for i in range(n):
                 #print(i, j, ni, nj)
                 pass
 
-result = (9>>1)
-print(result)
+
+### 순차검색 정렬안됨
+def sequentialSearch(a, n, key):
+    # a 값 찾을 리스트
+    # n 리스트 길이
+    # key 값
+    i = 0
+    while i<n and a[i] != key:  # 단축평가, 유효범위 밖이면 일단 멈춤
+        i += 1
+    if i <n: return i  # a[i] == key 여서 반복문 빠졌으면
+    else: return -1  # i<n이 아니게 되어서 반복문 빠졌으면
+
+
+### 순차검색 정렬됨
+def sequentialSearch2(a, n, key):
+    i = 0
+    while i<n and a[i] < key:  # 범위 안, 키값보다 작으면 계속
+        i += 1
+    if i <n and a[i] == key:  # 그 전에 찾으면 값 리턴
+        return i
+    else: return -1 # 범위 밖, 키값보다 커지면 -1 리턴
+
+### 이진 검색
+def binarySearch(a, n, key):
+    # a 값 찾을 리스트
+    # n 리스트 길이
+    # key 값
+    start = 0
+    end = n-1
+    while start <= end:
+        middle = (start + end) // 2
+        if a[middle] == key:  # 검색 성공
+            return True
+        elif a[middle] > key:  # 중앙이 목표보다 더 큼
+            end = middle - 1
+        else:                  # 중앙이 목표보다 작음
+            start = middle + 1
+    return False  # 검색 실패
+
+## 이진 검색 재귀함수
+
+def binarySearch2(a, low, high, key):
+    if low > high:
+        return False
+    else:
+        middle = (low + high) // 2
+        if key == a[middle]:
+            return True
+        elif a[middle] > key:
+            return binarySearch2(a, low, middle - 1, key)
+        elif a[middle] < key:
+            return binarySearch2(a, middle + 1, high, key)
--- algorithm/sorting.py
+++ algorithm/sorting.py
@@ -13,17 +13,35 @@ def Counting_sort(A, k):
     # C[] 카운트 배열
     B = [0] * len(A)
     C = [0] * (k+1)
-    for i in range(0, len(A)):
+    for i in range(0, len(A)):  # 각 값 개수 세기
         C[A[i]] += 1
     
-    for i in range(1, len(C)):
+    for i in range(1, len(C)):  # 누적합
         C[i] += C[i - 1]
     
-    for i in range(len(B) - 1, -1, -1):
+    for i in range(len(B) - 1, -1, -1):  # 개수 빼고 그 위치에 삽입
         C[A[i]] -= 1
         B[C[A[i]]] = A[i]
     return B
 
-a = [1,2,16,4,7,13,9]
-#bubble_sort(lst, rev= True)
-#print(Counting_sort(a,max(a)))
+
+def selection_sort(a, N):
+    for i in range(N-1):
+        minIdx = i  # 최소인덱스 정의
+        for j in range(i+1, N):  # 최소인덱스 찾기
+            if a[minIdx] > a[j]: 
+                minIdx = j
+        # 리스트에서 최소값 찾았으니 위치 바꾸기
+        a[i], a[minIdx] = a[minIdx], a[i]
+    return a
+
+## 셀렉션 알고리즘
+def select(arr, k):
+    for i in range(0, k):  # 위와 달리 목표 k번째 까지만 선택함
+        minIdx = i
+        for j in range(i+1, len(arr)):
+            if arr[minIdx] > arr[j]:
+                minIdx = j
+        arr[i], arr[minIdx] = arr[minIdx], arr[i]
+
+    return arr[k-1]  # k 번째로 작은 목표값 반환
