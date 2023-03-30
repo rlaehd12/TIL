f16b784c4f272b243e548797b529f5f6cc85fa0f
rlaehd12 <dnpffldxjs12@gmail.com>
2023-03-30 13:52:39 +0900

    123

--- algorithm/0216subset.py
+++ algorithm/0216subset.py
@@ -1,5 +1,5 @@
-key = 20
-a = 3
+key = 6
+a = 6
 A = [i for i in range(a)]
 N = len(A)
 bit = [0] * N  # bit[i] A[i]원소가 부분집합에 포함되는지 표시
@@ -54,6 +54,15 @@ def f_re(i, k, key):
         if s == key:
             return 1
         return 0
+
+    # if i == k:  # base case, 배열 크기만큼 오면 확인
+    #     if s == t:
+    #         # for j in range(k):  # 출력용 코드
+    #         #     if bit[j]:
+    #         #         print(A[j], end=' ')
+    #         # print()
+    #         cnt += 1
+    #     return
     else:
         bit[i] = 1
         if f_re(i+1, k, key):
@@ -71,37 +80,32 @@ print('###################')
 # 백트래킹, 부분집합 합이 목표보다 크면 고려할 필요 없음
 # 가지치기 시행
 # i원소, k집합 크기, s i-1까지 고려된 합, t 목표
-def f_back(i, k, s, t): 
+def f_back(i, k, s, t, rs): 
     global cnt
     global fcnt
     fcnt += 1  # 함수 호출시마다 +1
 
-    #  가지치기
+    #  가지치기=========================
     if s > t:  # 이미 합이 목표값보다 크면
         return 
     # 남은 원소 고려할 필요 없는 경우
     elif s == t:  # 이미 목표와 같으면
         cnt += 1
+        print(bit)
         return
+    elif s+rs <key:  # 나머지 원소 고려해도 답에 도달 못하면
+        return
+
     elif i == k:  # 모든 원소 고려
         return
 
-
-    # if i == k:  # base case, 배열 크기만큼 오면 확인
-    #     if s == t:
-    #         # for j in range(k):  # 출력용 코드
-    #         #     if bit[j]:
-    #         #         print(A[j], end=' ')
-    #         # print()
-    #         cnt += 1
-    #     return
     else:
         bit[i] = 1
-        f_back(i+1, k, s+A[i], t)  # A[i]포함
+        f_back(i+1, k, s+A[i], t, rs-A[i])  # A[i]포함
         bit[i] = 0
-        f_back(i+1, k, s+0, t)  # A[i] 미포함
+        f_back(i+1, k, s+0, t, rs-A[i])  # A[i] 미포함
 
 bit = [0] * N
 fcnt = 0
-f_back(0, N, 0, key)
+f_back(0, N, 0, key, sum(A))
 print(cnt, fcnt)  # 목표 개수, 함수 호출 횟수
--- algorithm/tree.py
+++ algorithm/tree.py
@@ -74,24 +74,77 @@ def deq():
     return return_val
 
 
-heap = [0] * 101  # 100개짜리 완전이진트리 
-last = 0
+#========================================================최소 힙 클래스 구현
+class stack():
 
-enq(3)
-enq(4)
-enq(5)
-enq(64)
-enq(32)
-enq(11)
-enq(17)
-enq(121)
-print(deq())
-print(deq())
-print(deq())
-print(deq())
-print(deq())
-print(deq())
-print(deq())
-print(deq())
+    def __init__(self, n):
+        self.lst = [0] * n
+        self.top = -1
+        self.size = n
+    
+    def isempty(self):
+        if self.top == -1:
+            return True
+        else:
+            return False
+    
+    def push(self, item):
+        self.top += 1
+        self.lst[self.top] = item
+    
+    def pop(self):
+        if self.isempty():
+            pass
+        else:
+            self.top -= 1
+            return self.lst[self.top + 1]
+    
+    def peek(self):
+        if self.isempty():
+            pass
+        else:
+            return self.lst[self.top]
 
-print(heap[1:last+1])
+class heap(stack):  # 최소 힙
+
+    def __init__(self, n):
+        super().__init__(n+1)
+        self.top = 0
+    
+    def isempty(self):
+        if self.top == 0:
+            return True
+        else:
+            return False
+    
+    def enq(self, n):
+        self.push(n)
+        c = self.top
+        p = c//2
+        while p > 0 and self.lst[c] < self.lst[p]:
+            self.lst[c], self.lst[p] = self.lst[p], self.lst[c]
+            c = p
+            p = c//2
+    
+    def deq(self):
+        if self.isempty():
+            return
+
+        temp = self.lst[1]
+        self.lst[1] = self.pop()
+
+        p = 1
+        c = p*2
+
+        while c <= self.top:
+            if c+1 <= self.top and self.lst[c] > self.lst[c+1]:
+                c += 1
+            
+            if self.lst[c] < self.lst[p]:
+                self.lst[c], self.lst[p] = self.lst[p], self.lst[c]
+            else:
+                break
+
+            p = c
+            c = p*2
+        return temp
